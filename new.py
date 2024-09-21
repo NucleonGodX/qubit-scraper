0ef3350aaf2724e187206d11e3bcffcf55271412
NucleonGodX <racerpro41@gmail.com>
2024-09-21 18:45:15 +0530

    cisco added

--- new.py
+++ new.py
@@ -4,6 +4,7 @@ from nvd_scraper.spiders.ibm import IBMVulnerabilitySpider
 from nvd_scraper.spiders.qnap import QNAPAdvisorySpider
 from nvd_scraper.spiders.wordfence import WordFenceVulnerabilitySpider
 from nvd_scraper.spiders.microsoft import MicrosoftVulnerabilitySpider
+from nvd_scraper.spiders.cisco import CiscoAdvisorySpider
 
 def run_second_level_scraping():
     process = CrawlerProcess()
@@ -11,6 +12,7 @@ def run_second_level_scraping():
     process.crawl(QNAPAdvisorySpider)
     process.crawl(WordFenceVulnerabilitySpider)
     process.crawl(MicrosoftVulnerabilitySpider)
+    process.crawl(CiscoAdvisorySpider)
     process.start()
 
 if __name__ == "__main__":
